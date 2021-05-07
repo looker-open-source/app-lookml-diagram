c4c9fa8ded102a89c1a6329b487aaebc0478987a
noahamac <56001784+noahamac@users.noreply.github.com>
2021-05-06 17:59:37 -0700

    fix: updates helppanel tests and removes others (#50)
    * chore: updates diagram generation tests
    
    * initial prettier attempt
    
    * adds final pretty finals
    
    * removes files and enables lint action
    
    * updates snapshot
    
    * fix: updates helppanel tests and removes others
    
    Co-authored-by: Noah MacDonald <noahmac@google.com>

--- src/__tests__/test-utils/render_with_extension.tsx
+++ src/__tests__/test-utils/render_with_extension.tsx
@@ -1,19 +1,14 @@
 /*
-
  MIT License
-
- Copyright (c) 2020 Looker Data Sciences, Inc.
-
+ Copyright (c) 2021 Looker Data Sciences, Inc.
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
-
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,38 +16,35 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
-
  */
 
-import React from 'react';
-import { DiagramToolbar } from '../../components/DiagramFrame/DiagramCanvas/DiagramToolbar'
-import { shallow } from 'enzyme';
-import 'jest-styled-components'
+import React, { ReactElement } from 'react'
+import { renderWithTheme } from '@looker/components-test-utils'
+import {  ExtensionContext2, ExtensionContextData2 } from "@looker/extension-sdk-react"
+import {  LookerHostData,  registerHostApi, ExtensionHostApi } from "@looker/extension-sdk"
+import { Looker40SDK } from '@looker/sdk'
 
-// jest.mock("@looker/components", () => ({
-//   Divider: () => "Divider",
-//   Flex: () => "Flex",
-//   FlexItem: () => "FlexItem",
-//   Card: () => "Card",
-//   IconButton: () => "IconButton",
-//   theme: {colors: {key:"rgb(45, 126, 234)"}, space: {large: "2em"}, fontSizes: {large: "2em"}, fontWeights: {normal: "1em"}}
-// }))
+const withExtensionContext = (
+  consumer: ReactElement<any>
+) => {
+  const extensionSDK = {
+    lookerHostData: {} as Readonly<LookerHostData>,
+    error: (_: ErrorEvent) => {}
+  } as ExtensionHostApi
+  registerHostApi(extensionSDK)
+  const extensionContext: ExtensionContextData2<Looker40SDK> = {
+    extensionSDK,
+    coreSDK: {} as Looker40SDK,
+    route: ''
+  }
+  return (
+    <ExtensionContext2.Provider value={extensionContext}>
+      {consumer}
+    </ExtensionContext2.Provider>
+  )
+}
 
-describe('<DiagramToolbar />', () => {
-  const basic = shallow(
-  <DiagramToolbar
-    zoomFactor={1}
-    reload={false}
-    defaultMinimap={true}
-    minimapEnabled={true}
-    minimapUntoggled={true}
-    setZoomFactor={undefined}
-    setViewPosition={undefined}
-    setReload={undefined}
-    setMinimapUntoggled={undefined}
-    setMinimapEnabled={undefined}
-  />);
-  it('should match the basic', () => {
-    expect(basic.debug()).toMatchSnapshot();
-  });
-});
+export const renderWithExtensionContext = (
+  component: ReactElement<any>
+) =>
+  renderWithTheme(withExtensionContext(component))
