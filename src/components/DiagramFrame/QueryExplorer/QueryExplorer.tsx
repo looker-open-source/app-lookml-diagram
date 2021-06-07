d128b8d1d8533788c0ec9b3b457c804555113903
noahamac <56001784+noahamac@users.noreply.github.com>
2021-06-07 08:30:07 -0700

    adds queryexplorer (#53)
    Co-authored-by: Noah MacDonald <noahmac@google.com>

---
+++ src/components/DiagramFrame/QueryExplorer/QueryExplorer.tsx
@@ -0,0 +1,93 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2021 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import React from 'react'
+import {
+  SpaceVertical,
+  Aside,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+  ExtendComponentsThemeProvider,
+  theme,
+  Text
+} from '@looker/components'
+import styled from 'styled-components'
+import { DiagrammedModel } from '../../../utils/LookmlDiagrammer'
+
+export const QueryExplorerPanel = styled(Aside)`
+  border-left: solid 1px ${props => props.theme.colors.ui2};
+  overflow-y: auto;
+  background-color: ${props => props.theme.colors.background};
+  z-index: 0;
+`
+QueryExplorerPanel.defaultProps = {
+  width: `700px`,
+  px: 'medium',
+  py: 'large'
+}
+
+const StyledTabs = styled(TabList)`
+  width: 100%;
+`
+const StyledText = styled(Text)`
+  color: ${props => props.theme.colors.text};
+`
+
+export const QueryExplorer: React.FC<{
+  queryFields: string[]
+  diagramMetadata: DiagrammedModel
+}> = ({ queryFields, diagramMetadata }) => {
+  return (
+    <ExtendComponentsThemeProvider
+      themeCustomizations={{
+        colors: {
+          background: theme.colors.text,
+          text: theme.colors.background,
+        },
+      }}
+    >
+      <QueryExplorerPanel>
+        <SpaceVertical gap="medium">
+        <Tabs>
+          <StyledTabs distribute>
+            <Tab>Visualization</Tab>
+            <Tab>Data</Tab>
+            <Tab>Code</Tab>
+          </StyledTabs>
+          <TabPanels width='100%'>
+            <TabPanel><StyledText>TODO: Visualization Area and Editor</StyledText></TabPanel>
+            <TabPanel><StyledText>TODO: DataTable</StyledText></TabPanel>
+            <TabPanel><StyledText>TODO: CodeDisplay</StyledText></TabPanel>
+          </TabPanels>
+        </Tabs>
+        </SpaceVertical>
+      </QueryExplorerPanel>
+    </ExtendComponentsThemeProvider>
+  )
+}
