9e142703885c38c5d43a36768bb2a5c2c4df12aa
Bryn Ryans <bryans99@users.noreply.github.com>
2021-12-01 11:23:23 -0800

    chore: upgrade build to webpack 5 and babel (#67)
    1. upgraded dependencies including react and looker components
    2. dependencies are now split out from dev dependencies (some may may
    been missed but this will not cause an issue)
    3. webpack upgraded
    4. babel added
    5. fixed exports that cause compiler warnings
    6. converted test_setup.js to use requires (could not figure out how to
    get transformation to work in a reasonable amount of time).
    7. Updated README to reflect currently reality. Cleaned it up a bit.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,52 @@
-const path = require("path")
-const fs = require('fs')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+const path = require("path");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+const PATHS = {
+  app: path.join(__dirname, "src/index.tsx"),
+};
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: [
-          /node_modules/,
-          path.resolve(__dirname, "src/__tests__")
-        ],
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        include: /src/,
+        sideEffects: false,
       },
       {
         test: /\.css$/i,
@@ -22,26 +56,16 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: 'file-loader',
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: { buffer: false },
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
+  devtool: "source-map",
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: process.env.ANALYZE_MODE || 'disabled',
-    })
+      analyzerMode: process.env.ANALYZE_MODE || "disabled",
+    }),
   ],
-  devServer: {
-    index: 'index.html',
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    },
-  },
-}
+};
