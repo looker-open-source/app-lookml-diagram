945e824328664b82c0aa1e7ebca06fc0d54ee8a2
Juventin <jeremy.juv@gmail.com>
2024-11-15 17:17:34 +0100

    feat: run generateExploreDiagram only on selected explore, and use exploreName in queryCacheKey Co-authored-by: name <jeremy.juv@gmail.com>

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -13,6 +13,7 @@ import type {
 } from '@looker/sdk/lib/4.0/models'
 import type { DiagrammedModel } from './LookmlDiagrammer'
 import { generateModelDiagrams } from './LookmlDiagrammer'
+import { usePathNames } from './routes'
 
 export interface DetailedModel {
   model: ILookmlModel
@@ -116,11 +117,22 @@ export function useModelDiagrams(
   // Removing git branches as it causes a double render
   const tempModelDetail = { ...modelDetail } as Partial<DetailedModel>
   delete tempModelDetail.gitBranches
+
+  const { exploreName } = usePathNames()
   const queryCacheKey =
-    JSON.stringify(tempModelDetail) + hiddenToggle + displayFieldType
+    JSON.stringify(tempModelDetail) +
+    hiddenToggle +
+    displayFieldType +
+    exploreName
   const { data } = useQuery(
     queryCacheKey,
-    () => generateModelDiagrams(modelDetail, hiddenToggle, displayFieldType),
+    () =>
+      generateModelDiagrams(
+        modelDetail,
+        hiddenToggle,
+        displayFieldType,
+        exploreName
+      ),
     {
       ...defaultQueryOptions,
       initialData: undefined,
